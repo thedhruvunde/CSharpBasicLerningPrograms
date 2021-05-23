46864c2a5ff41fee7b0077befba64c8fbc52e45c
Dhruv Unde <dhruvcode413@gmail.com>
2021-05-23 10:34:33 +0530

    Classes Program
    This is the last one

---
+++ Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes
+{
+	class ArithematicOperations
+	{
+		public void SquareIt()
+		{
+			while (true)
+			{
+				Console.Write("Type to square> ");
+				int inp = Convert.ToInt32(Console.ReadLine());
+				int result = inp * inp;
+				Console.WriteLine("Square is "+result);
+			}
+		}
+	}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithematicOperations num = new ArithematicOperations();
+            num.SquareIt();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# CSharpBasicLerningPrograms
+# C# Basic Learning Programs
 C# programs for beginners
+
+## Requirenments
+
+# .Net
+
+## How to run
+
+# Steps-
+    1) Open a Command prompt/Terminal window in the directory
+    2) Then simply run command 'dotnet run'
+    Remember you will not be able to learn C# by run the program. For detailed description of program open Program.cs File :D  
